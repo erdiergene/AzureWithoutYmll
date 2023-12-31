116ab4b84d80c536471b7e2bc349cde258ac45d6
erdi ergene <erdieergene@gmail.com>
2023-12-31 17:15:42 +0300

    TEST

--- AzureWithoutYml/ViewController.swift
+++ AzureWithoutYml/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         //
+        //TEST
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
